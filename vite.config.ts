6da823f0632889a9999455c7d3ed70367a3a90d0
silverwind <me@silverwind.io>
2025-04-06 01:04:49 +0200

    updates

--- vite.config.ts
+++ vite.config.ts
@@ -1,9 +1,7 @@
 import {defineConfig} from "vite";
 import {nodeLib} from "vite-config-silverwind";
-import {dtsPlugin} from "./index.ts";
 
 export default defineConfig(nodeLib({
   url: import.meta.url,
-  dts: false,
-  plugins: [dtsPlugin({args: ["--project", "tsconfig.types.json"]})],
+  dtsOpts: {args: ["--project", "tsconfig.types.json"]},
 }));
